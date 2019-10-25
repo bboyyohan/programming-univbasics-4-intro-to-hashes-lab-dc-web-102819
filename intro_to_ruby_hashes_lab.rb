a0432cf90f2c5fc8b6065367724efec4762466c1
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-10-25 00:43:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  
+  read_from_hash = {hash => key}
+  hash[key]
   # return the correct value using the hash and key parameters
 end
 
